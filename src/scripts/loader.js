17a990da675e59d6ca8fb62b0ef2b77445df92f2
snagy22000 <sebastian.nagy@outlook.com>
2017-03-14 07:33:17 +0100

    ADD - loader js file, and html,css - npm start script for browser-sync

---
+++ src/scripts/loader.js
@@ -0,0 +1,16 @@
+(function () {
+  "use strict";
+
+  window.addEventListener('load', hide_delete_Loader);
+
+  // set opacity to 0
+  function hide_delete_Loader() {
+    var loadContainer = document.querySelector('.loading-container');
+    loadContainer.style.opacity = 0;
+
+    setTimeout(function () {
+      // delete load container after transition
+      document.body.removeChild(loadContainer);
+    }, 5000);
+  }
+})();
